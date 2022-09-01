905b3e9ee917133d2b3d22897368dad4284f8ab2
firefoxic <firefoxic.dev@gmail.com>
2022-09-01 18:29:46 +0300

    Update test icons

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ import gulp from "gulp"
 
 const { src, dest } = gulp
 
-export function build () {
-	return src(`test/src/icons/**/*.svg`)
+export function test () {
+	return src([`test/src/icons/**/*.svg`, `!test/src/icons/**/*-*.svg`])
 		.pipe(stacksvg())
 		.pipe(dest(`test/dest`))
 }
